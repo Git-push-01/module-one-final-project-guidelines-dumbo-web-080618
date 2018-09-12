c6c885467f4768ec1c7ef28f3de1b325c80db35d
Malcome Fein <mfein90@gmail.com>
2018-09-12 09:06:44 -0400

    new commit sep 12

--- bin/run.rb
+++ bin/run.rb
@@ -18,10 +18,11 @@ def create_team
 
   team_name = gets.chomp
 puts
-  Team.create(name: team_name)
+  a_team=Team.create(name: team_name)
     puts "#{team_name} cool!!!"
     puts
     puts "You created #{team_name}!"
+    a_team
 end
 
 # puts "pick your players for your team"
@@ -33,69 +34,29 @@ def show_all_players
   end
 end
 
-# def pick_player(player_name)
-#   Player.all.find_by do |player|
-#     player.name = player_name
-#   end
-# end
+
+def add_a_player_to_a_team(a_player, team)
+
+  bob= Player.find_by(name: a_player)
+
+  bob.add_to_team(team)
+end
+
+
 
 
 
 #executables
 create_league
-create_team
+team=create_team
 show_all_players
+puts
+puts
+puts "Select a player"
+puts "When you are done plese press N"
+a_player = gets.chomp
+puts
+puts
+add_a_player_to_a_team(a_player, team)
+
 # pick_player(player_pick)
-
-
-
-
-#
-# puts
-# Team.all.each do |team|
-#   puts "Welcome To NFL Player Stats"
-# end
-# puts
-#
-# team = Team.find(1)
-#
-# puts"Pick a Team "
-# puts
-#
-# puts"Pick a Player see the Stats"
-# all_players = team.players
-# puts
-# all_players.each do |player|
-#   puts "#{player.name}"
-#
-# end
-#
-# # Status Key
-# puts "ACT = Active"
-# puts "RES = Injured reserve"
-# puts "NON = Non football related injured reserve"
-# puts "SUS = Suspended"
-# puts "PUP = Physically unable to perform"
-# puts "UDF = Unsigned draft pick"
-# puts "EXE = Exempt"
-#
-#
-# puts
-# player_name = gets.chomp
-# puts
-# puts
-# result = Player.find_by(name: player_name)
-# puts "Player Name: #{result.name}"
-# puts "Player Number: #{result.player_number}"
-# puts "Player Status: #{result.player_status}"
-# puts "Player Birth Date: #{result.birth_date}"
-# puts "Player Height: #{result.height}"
-# puts "Player Weight: #{result.weight}"
-# puts "Player College: #{result.college}"
-# puts
-# puts
-# puts"Thank you come again"
-# puts
-# puts
-# binding.pry
-# 0
--- lib/model/player.rb
+++ lib/model/player.rb
@@ -1,4 +1,9 @@
 class Player < ActiveRecord::Base
   # has_many :player_cards
   belongs_to :team
+
+  def add_to_team(team)
+    self.team = team
+    self.save
+  end
 end
